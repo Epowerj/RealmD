cf54ed9cd8f73a2c08a964cca5e46fe5f8d51f72
Epowerj <epowerj@gmail.com>
2017-10-24 16:44:14 -0400

    Fixed formatting

--- test.py
+++ test.py
@@ -12,28 +12,14 @@ def test_test():  # tests tests
     assert(2+2 == 4)  # hmm
 
 def test_chatinfo():
-    jsonparsed = json.loads("{'group_chat_created': False,
-    'caption_entities': [], 'photo': [], 'new_chat_members': [],
-    'chat': {'type': 'private', 'id': 83218061, 'username': 'TesterUser', 'first_name': 'Tester'},
-    'date': 1508876530, 'text': 'Pong', 'channel_chat_created': False,
-    'message_id': 20, 'new_chat_photo': [], 'new_chat_member': None,
-    'entities': [], 'from': {'is_bot': True, 'id': 431157060, 'username':
-    'RealmDbot', 'first_name': 'RealmD'}, 'supergroup_chat_created': False,
-    'delete_chat_photo': False}");
+    jsonparsed = json.loads("{'update_id': 389907451, 'message': {'group_chat_created': False, 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'chat': {'type': 'private', 'id': 83218061, 'username': 'Epowerj', 'first_name': 'EspookyJ'}, 'date': 1508876530, 'text': '/chatinfo', 'channel_chat_created': False, 'message_id': 19, 'new_chat_photo': [], 'new_chat_member': None, 'entities': [{'type': 'bot_command', 'length': 5, 'offset': 0}], 'from': {'is_bot': False, 'id': 83218061, 'first_name': 'Epowerj', 'username': 'Epowerj', 'language_code': 'en-US'}, 'supergroup_chat_created': False, 'delete_chat_photo': False}}");
 
     u = Update.de_json(jsonparsed , b)
 
     bot.chatinfo(b, u)
 
 def test_ping():
-    jsonparsed = json.loads("{'group_chat_created': False,
-    'caption_entities': [], 'photo': [], 'new_chat_members': [],
-    'chat': {'type': 'private', 'id': 83218061, 'username': 'Epowerj', 'TesterUser': 'Tester'},
-    'date': 1508876530, 'text': 'Pong', 'channel_chat_created': False,
-    'message_id': 20, 'new_chat_photo': [], 'new_chat_member': None,
-    'entities': [], 'from': {'is_bot': True, 'id': 431157060, 'username':
-    'RealmDbot', 'first_name': 'RealmD'}, 'supergroup_chat_created': False,
-    'delete_chat_photo': False}");
+    jsonparsed = json.loads("{'update_id': 389907451, 'message': {'group_chat_created': False, 'caption_entities': [], 'photo': [], 'new_chat_members': [], 'chat': {'type': 'private', 'id': 83218061, 'username': 'Epowerj', 'first_name': 'EspookyJ'}, 'date': 1508876530, 'text': '/chatinfo', 'channel_chat_created': False, 'message_id': 19, 'new_chat_photo': [], 'new_chat_member': None, 'entities': [{'type': 'bot_command', 'length': 5, 'offset': 0}], 'from': {'is_bot': False, 'id': 83218061, 'first_name': 'Epowerj', 'username': 'Epowerj', 'language_code': 'en-US'}, 'supergroup_chat_created': False, 'delete_chat_photo': False}}");
 
     u = Update.de_json(jsonparsed , b)
 
