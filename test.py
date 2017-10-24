22bef4718707317e062bdf87b3c3835899c921c9
Epowerj <epowerj@gmail.com>
2017-10-24 15:58:16 -0400

    Added some tests not sure if they'll work

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 
 import pytest
 import bot
+import os
 
-print("Running test.py")
+b = telegram.Bot(str(os.environ["TGKEY"]))
+u = telegram.Update(1243)
 
 def test_test():  # tests tests
     assert(2+2 == 4)  # hmm
+
+def test_chatinfo():
+    bot.chatinfo(b, u)
+
+def test_ping():
+    bot.ping(b, u)
